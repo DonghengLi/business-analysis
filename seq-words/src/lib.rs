a6b2bed58e99ccfbeaf8adbc61119725e142930a
DonghengLi <Dongheng_Li@asus.com>
2023-12-08 16:39:43 +0800

    improve seq-words doc

--- seq-words/src/lib.rs
+++ seq-words/src/lib.rs
@@ -1,52 +1,86 @@
-use serde::Deserialize;
-
-#[derive(Debug, Deserialize)]
-struct Test {
-    pub doctor: String,
-    pub division: String,
-    pub sentence: String,
-    pub frequency: String,
-    pub encounter_ids: String,
+/// Maintaining the sentence words sequence in a ring buffer with a certain number of words.
+/// The sequence starts with a word and ends with word, with non-words (e.g. punctuation or space) in between.
+pub struct SentenceRingBuffer {
+    /// (word, true - a real word, false - non-word)
+    words: Vec<(String, bool)>,
+    head: usize,
+    tail: usize,
+    /// Number of words in the ring buffer.
+    num_words: usize,
+    /// Number of words in a sentence.
+    sentence_words_num: usize,
 }
 
-pub fn process_output() -> anyhow::Result<()> {
-    let mut reader = csv::Reader::from_path("output.csv")?;
-    let mut writer = csv::Writer::from_path("output-refined.csv")?;
-    writer.write_record(&["doctor", "division", "sentence", "frequency", "encounter_ids"])?;
-
-    let records = reader.deserialize()
-        .collect::<csv::Result<Vec<Test>>>()?;
-
-    let mut removed: Vec<bool> = vec![false; records.len()];
-    for i in 0..records.len() {
-        if removed[i] {
-            continue;
-        }
-        let record1 = &records[i];
-        for j in (i+1)..records.len() {
-            if i == j || removed[j] {
-                continue;
-            }
-            let record2 = &records[j];
-            if record1.doctor == record2.doctor 
-                && record1.division == record2.division 
-                && record1.frequency == record2.frequency 
-            {
-                if record1.sentence.contains(&record2.sentence) {
-                    removed[j] = true;
-                }
-                if record2.sentence.contains(&record1.sentence) {
-                    removed[i] = true;
-                }
-            }
+impl SentenceRingBuffer {
+    pub fn new(sentence_words_num: usize) -> Self {
+        Self {
+            words: vec![(String::new(), false); sentence_words_num * 2],
+            head: 0,
+            tail: 0,
+            num_words: 0,
+            sentence_words_num,
         }
     }
-    for i in 0..records.len() {
-        if removed[i] {
-            continue;
-        }
-        let record = &records[i];
-        writer.write_record(&[&record.doctor, &record.division, &record.sentence, &record.frequency, &record.encounter_ids])?;
+
+    fn next_index(&self, index: usize) -> usize {
+        (index + 1) % self.words.len()
     }
-    Ok(())
+
+    fn create_current_sentence(&self) -> String {
+        let mut sentence = String::new();
+
+        let mut scan_index = self.head;
+        while scan_index != self.tail {
+            sentence.push_str(&self.words[scan_index].0);
+            scan_index = self.next_index(scan_index);
+        }
+
+        sentence
+    }
+
+    /// Add a word into the ring buffer.
+    /// If there are enough words for a sentence in the ring buffer, the sentence is returned.
+    pub fn add(&mut self, word: String, is_word: bool) -> Option<String> {
+        let mut sentence: Option<String> = None;
+        if is_word {
+            // A new word added
+            if self.num_words == self.sentence_words_num {
+                // There are already enough words in the ring buffer.
+                // Pop the last words.
+                self.head = self.next_index(self.head);
+                // Pop the last non-word.
+                if self.head != self.tail {
+                    let head_word = &self.words[self.head];
+                    if !head_word.1 {
+                        self.head = self.next_index(self.head);
+                    }
+                }
+            } else {
+                // There are not enough words in the ring buffer. Just simply adds the word.
+                self.num_words += 1;
+            }
+            self.words[self.tail] = (word, is_word);
+            self.tail = self.next_index(self.tail);
+
+            if self.num_words == self.sentence_words_num {
+                sentence = Some(self.create_current_sentence());
+            }
+        } else if self.head != self.tail {
+            // Only add non-word if there are words in the ring buffer.
+            let tail_word = if self.tail == 0 {
+                self.words.last_mut().unwrap()
+            } else {
+                &mut self.words[self.tail - 1]
+            };
+            if tail_word.1 {
+                // Push the non-word if the last is a word.
+                self.words[self.tail] = (word, is_word);
+                self.tail = self.next_index(self.tail);
+            } else {
+                // Concat the non-word if the last is a non-word.
+                tail_word.0.push_str(&word);
+            }
+        }
+        sentence
+    }
 }
--- seq-words/src/main.rs
+++ seq-words/src/main.rs
@@ -1,7 +1,13 @@
-use std::collections::{HashMap, HashSet};
+#![allow(clippy::needless_range_loop)]
 
 use serde::Deserialize;
+use std::{
+    collections::{HashMap, HashSet},
+    path::{Path, PathBuf},
+    rc::Rc, io::Write,
+};
 
+/// Input CSV record.
 #[derive(Debug, Deserialize)]
 struct Record {
     #[serde(rename = "encounterId")]
@@ -12,16 +18,25 @@ struct Record {
     processed_soap: String,
 }
 
+/// Key to group the records.
 #[derive(PartialEq, Eq, Hash, Debug)]
 struct Key {
     doctor: String,
     division: String,
 }
 
-const WHITE_SPACE: u32 = 1;
-const WORD: u32 = 2;
-const OTHER: u32 = 4;
+/// The values for each key.
+struct Value {
+    encounter_id: Rc<str>,
+    soap: Vec<Vec<(String, u32)>>,
+}
 
+// Define aligned with the output data of [../soap-sep]
+// const WHITE_SPACE: u32 = 1;
+const WORD: u32 = 2;
+// const OTHER: u32 = 4;
+
+// Subject CSV record.
 #[derive(Debug, Deserialize)]
 struct SubjectInfo {
     #[serde(rename = "SUBJECT_ID")]
@@ -30,88 +45,203 @@ struct SubjectInfo {
     subject_name: String,
 }
 
-fn load_subject() -> anyhow::Result<HashMap<String, String>> {
-    let mut reader = csv::Reader::from_path("subject.csv")?;
+/// Load the subject maps from the CSV file.
+fn load_subject(path: &Path) -> anyhow::Result<HashMap<String, String>> {
+    let mut reader = csv::Reader::from_path(path)?;
     let mut map = HashMap::new();
     for record in reader.deserialize() {
-        let SubjectInfo { subject_id, subject_name } = record?;
+        let SubjectInfo {
+            subject_id,
+            subject_name,
+        } = record?;
         map.insert(subject_id, subject_name);
     }
     Ok(map)
 }
 
-const MIN_WORDS_NUM: usize = 4;
-const MAX_WORDS_NUM: usize = 10;
+fn main() -> anyhow::Result<()> {
+    let matches = clap::Command::new("seq-words")
+        .version("0.1.0")
+        .author("Dongheng Li")
+        .about("Finds the repeated sentences in the SOAP notes.")
+        .arg(
+            clap::Arg::new("min_words")
+                .short('m')
+                .long("min-words")
+                .value_name("MIN_WORDS")
+                .help("Sets the minimum number of words in a sentence")
+                .value_parser(clap::value_parser!(usize))
+                .required(true),
+        )
+        .arg(
+            clap::Arg::new("input")
+                .short('i')
+                .long("input")
+                .value_name("INPUT_FILE")
+                .value_parser(clap::value_parser!(PathBuf))
+                .help("Sets the input CSV file path")
+                .required(true),
+        )
+        .arg(
+            clap::Arg::new("output")
+                .short('o')
+                .long("output")
+                .value_name("OUTPUT_FILE")
+                .value_parser(clap::value_parser!(PathBuf))
+                .help("Sets the output CSV file path")
+                .required(true),
+        )
+        .arg(
+            clap::Arg::new("subject")
+                .short('s')
+                .long("subject")
+                .value_name("SUBJECT_FILE")
+                .value_parser(clap::value_parser!(PathBuf))
+                .help("Sets the subject mapping CSV file path")
+                .required(true),
+        )
+        .get_matches();
 
-fn get_output() -> anyhow::Result<()> {
-    let subject_map = load_subject()?;
+    let min_words: usize = *matches.get_one("min_words").unwrap();
+    let input_file: &Path = matches.get_one::<PathBuf>("input").unwrap();
+    let output_file: &Path = matches.get_one::<PathBuf>("output").unwrap();
+    let subject_file: &Path = matches.get_one::<PathBuf>("subject").unwrap();
 
-    let mut reader = csv::Reader::from_path("sample.csv")?;
-    let mut buffer = String::new();
-    let mut data: HashMap<Key, HashMap<String, Vec<String>>> = HashMap::new();
+    let subject_map = load_subject(subject_file)?;
 
-    // TODO: remove this at root.
-    let mut encounter_ids: HashSet<String> = HashSet::new();
-    
-    for record in reader.deserialize() {
-        let Record { encounter_id, doctor, division, processed_soap  } = record?;
-        if encounter_ids.contains(&encounter_id) {
-            continue;
-        }
-        encounter_ids.insert(encounter_id.clone());
-
-        let soap: Vec<Vec<(String, u32)>> = serde_json::from_str(&processed_soap)?;
-        let key = Key {
-            doctor, division,
+    // Loads the data and group them by doctor and division.
+    let mut records = HashMap::new();
+    for record in csv::Reader::from_path(input_file)?.deserialize() {
+        let Record {
+            encounter_id,
+            doctor,
+            division,
+            processed_soap,
+        } = record?;
+        let encounter_id: Rc<str> = encounter_id.into();
+        let key = Key { doctor, division };
+        let value = Value {
+            encounter_id,
+            soap: serde_json::from_str(&processed_soap)?,
         };
-        
-        let sentences = data.entry(key).or_insert_with(HashMap::new);
-        
-        for words in &soap {
-            for i in 0..words.len() {
-                if words[i].1 != WORD {
-                    continue;
-                }
-                buffer.clear();
-                let mut words_num: usize = 0;
-                for j in i..words.len() {
-                    buffer.push_str(&words[j].0);
-                    
-                    if words[j].1 != WORD {
-                        continue;
-                    }
-                    
-                    words_num += 1;
-                    if words_num > MAX_WORDS_NUM {
-                        break;
-                    }
-                    if words_num >= MIN_WORDS_NUM {
-                        let encounter_ids = sentences.entry(buffer.clone()).or_insert_with(Vec::new);
-                        encounter_ids.push(encounter_id.clone());
-                    }
-                }
-            }
-        }
+        records.entry(key).or_insert_with(Vec::new).push(value);
     }
 
-    let mut writer = csv::Writer::from_path("output.csv")?;
-    writer.write_record(&["doctor", "division", "sentence", "frequency", "encounter_ids"])?;
+    let mut writer = std::fs::File::create(output_file)?;
+    // Write UTF-8 BOM to make Excel happy.
+    writer.write_all(&[0xEF, 0xBB, 0xBF])?;
+    let mut writer = csv::Writer::from_writer(writer);
+    writer.write_record([
+        "doctor",
+        "division",
+        "sentence",
+        "frequency",
+        "encounter_ids",
+    ])?;
+    // Process each doctor and division.
+    for (key, value) in records {
+        let repeated_min_words = get_repeated_min_words(&value, min_words);
+        let all_repeated_sentences =
+            get_all_repeated_sentences(&repeated_min_words, &value, min_words);
+        let repeated_sentences = remove_subset(all_repeated_sentences);
 
-    for (key, value) in data {
-        for (sentence, encounter_ids) in value {
+        for (sentence, encounter_ids) in repeated_sentences {
             let frequency = encounter_ids.len();
-            if frequency == 1 {
-                continue;
-            }
             let division = subject_map.get(&key.division).unwrap_or(&key.division);
             let encounter_ids: String = encounter_ids.join(", ");
-            writer.write_record(&[&key.doctor, division, &sentence, &frequency.to_string(), &encounter_ids])?;
+            writer.write_record([
+                &key.doctor,
+                division,
+                &sentence,
+                &frequency.to_string(),
+                &encounter_ids,
+            ])?;
         }
     }
     Ok(())
 }
 
-fn main() -> anyhow::Result<()> {
-    get_output()?;
-    seq_words::process_output()
+/// Get all the minimal number of words sentences that starts with a word and ends with a word.
+fn get_repeated_min_words(values: &[Value], min_words_num: usize) -> HashSet<String> {
+    let mut word_frequency = HashMap::<String, usize>::new();
+    for value in values {
+        for soap in &value.soap {
+            let mut buffer = seq_words::SentenceRingBuffer::new(min_words_num);
+            for (word, word_type) in soap {
+                if let Some(sentence) = buffer.add(word.clone(), *word_type == WORD) {
+                    *word_frequency.entry(sentence).or_insert(0) += 1;
+                }
+            }
+        }
+    }
+    // Create a set of sentences that have a frequency greater than 1.
+    word_frequency
+        .into_iter()
+        .filter(|(_, frequency)| *frequency > 1)
+        .map(|(sentence, _)| sentence)
+        .collect()
+}
+
+/// A maps of all the repeated sentences and the occurance of encounter IDs.
+/// If the sentence is repeated more than once in the same encounter, the encounter ID is added multiple times.
+fn get_all_repeated_sentences(
+    repeated_min_words: &HashSet<String>,
+    values: &[Value],
+    min_words_num: usize,
+) -> HashMap<String, Vec<Rc<str>>> {
+    let mut sentence_encounters = HashMap::new();
+    for value in values {
+        for soap in &value.soap {
+            for i in 0..soap.len() {
+                let mut full_sentence = String::new();
+                let mut buffer = seq_words::SentenceRingBuffer::new(min_words_num);
+
+                for j in i..soap.len() {
+                    let (word, word_type) = &soap[j];
+                    full_sentence.push_str(word);
+                    if let Some(sentence) = buffer.add(word.clone(), *word_type == WORD) {
+                        // There are enough words in the sentence when the buffer returns a sentence.
+                        if !repeated_min_words.contains(&sentence) {
+                            // If the sub-sentence between the last minimal number of words does not occur multiple time,
+                            // then we don't need to check the rest of the SOAP note.
+                            break;
+                        }
+                        sentence_encounters
+                            .entry(full_sentence.clone())
+                            .or_insert_with(Vec::new)
+                            .push(value.encounter_id.clone());
+                    }
+                }
+            }
+        }
+    }
+    sentence_encounters
+        .into_iter()
+        .filter(|(_, encounter_ids)| encounter_ids.len() > 1)
+        .collect()
+}
+
+/// Remove the subset sentence with no more frequency.
+fn remove_subset(input: HashMap<String, Vec<Rc<str>>>) -> Vec<(String, Vec<Rc<str>>)> {
+    let mut result: Vec<(String, Vec<Rc<str>>)> = Vec::new();
+    let mut input = input.into_iter().collect::<Vec<_>>();
+    // Sort the sentences by the lengh, so the possible subset of a sentence is always before the sentence.
+    input.sort_by(|(sentence1, _), (sentence2, _)| sentence1.len().cmp(&sentence2.len()));
+    for i in 0..input.len() {
+        let sentence = &input[i];
+        let mut is_subset = false;
+        for j in (i + 1)..input.len() {
+            let longer_sentence = &input[j];
+            if longer_sentence.1.len() >= sentence.1.len()
+                && longer_sentence.0.contains(&sentence.0)
+            {
+                is_subset = true;
+                break;
+            }
+        }
+        if !is_subset {
+            result.push(std::mem::take(&mut input[i]));
+        }
+    }
+    result
 }
